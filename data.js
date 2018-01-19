0170d9f99018e253a6e361b12d009d8069b45d4e
easychg <156375001@qq.com>
2018-01-19 15:39:36 +0800

    data

--- data.js
+++ data.js
@@ -1,7 +1,8 @@
 ﻿var data = [
-    { "url": "html/images/img-pro02.jpg", "name": "悦诗风吟石榴精华露套装50ml 改善暗沉", "tips": "自然色", "unit": "10" },
-    { "url": "html/images/img-pro02.jpg", "name": "悦诗风吟石榴精华露套装50ml 改善暗沉", "tips": "自然色", "unit": "10" },
-    { "url": "html/images/img-pro02.jpg", "name": "悦诗风吟石榴精华露套装50ml 改善暗沉", "tips": "自然色", "unit": "10" },
-    { "url": "html/images/img-pro02.jpg", "name": "悦诗风吟石榴精华露套装50ml 改善暗沉", "tips": "自然色", "unit": "10" },
-    { "url": "html/images/img-pro02.jpg", "name": "悦诗风吟石榴精华露套装50ml 改善暗沉", "tips": "自然色", "unit": "10" },
+    { "url": "html/images/img-pro02.jpg", "name": "板面", "tips": "自然色", "unit": "10" },
+    { "url": "html/images/img-pro02.jpg", "name": "驴肉火烧", "tips": "自然色", "unit": "10" },
+    { "url": "html/images/img-pro02.jpg", "name": "石锅拌饭", "tips": "自然色", "unit": "10" },
+    { "url": "html/images/img-pro02.jpg", "name": "快餐", "tips": "自然色", "unit": "10" },
+    { "url": "html/images/img-pro02.jpg", "name": "兰州拉面", "tips": "自然色", "unit": "10" },
+    { "url": "html/images/img-pro02.jpg", "name": "老堤北", "tips": "自然色", "unit": "10" },
 ];
