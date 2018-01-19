af45b139aee537ceda9e958fe0c700c776556628
easychg <156375001@qq.com>
2018-01-19 15:59:41 +0800

    add tel

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 ﻿var data = [
-    { "url": "html/images/img-pro02.jpg", "name": "板面", "tips": "自然色", "unit": "10" },
-    { "url": "html/images/img-pro02.jpg", "name": "驴肉火烧", "tips": "自然色", "unit": "10" },
-    { "url": "html/images/img-pro02.jpg", "name": "石锅拌饭", "tips": "自然色", "unit": "10" },
-    { "url": "html/images/img-pro02.jpg", "name": "快餐", "tips": "自然色", "unit": "10" },
-    { "url": "html/images/img-pro02.jpg", "name": "兰州拉面", "tips": "自然色", "unit": "10" },
-    { "url": "html/images/img-pro02.jpg", "name": "老堤北", "tips": "自然色", "unit": "10" },
+    { "url": "html/images/img-pro02.jpg", "name": "板面", "tips": "绿地商务城B座18楼", "unit": "10","tel":"18363228341" },
+    { "url": "html/images/img-pro02.jpg", "name": "驴肉火烧", "tips": "绿地商务城B座18楼", "unit": "10", "tel": "18363228341" },
+    { "url": "html/images/img-pro02.jpg", "name": "石锅拌饭", "tips": "绿地商务城B座18楼", "unit": "10", "tel": "18363228341" },
+    { "url": "html/images/img-pro02.jpg", "name": "快餐", "tips": "绿地商务城B座18楼", "unit": "10", "tel": "18363228341" },
+    { "url": "html/images/img-pro02.jpg", "name": "兰州拉面", "tips": "绿地商务城B座18楼", "unit": "10", "tel": "18363228341" },
+    { "url": "html/images/img-pro02.jpg", "name": "老堤北", "tips": "绿地商务城B座18楼", "unit": "10", "tel": "18363228341" },
 ];
