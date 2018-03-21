1978fb7b6b6b02ecde70cd6387043dfeb18a0e54
easychg <156375001@qq.com>
2018-03-21 16:41:17 +0800

    cccaaaaaaaa

---
+++ js/config.js
@@ -0,0 +1,42 @@
+﻿var config = {
+    "title": "",
+    "index": [1, 2, 3, 4, 5, 6, 7],
+    "lunbo": [
+        { "img": "images/1m.jpg", "alt": "酷站代码整理", "text": "图片1更详细的描述文字..." },
+        { "img": "images/2m.jpg", "alt": "酷站代码特效大全", "text": "图片2更详细的描述文字..." },
+        { "img": "images/3m.jpg", "alt": "酷站代码特效", "text": "图片3更详细的描述文字..." },
+        { "img": "images/4m.jpg", "alt": "酷站代码素材", "text": "图片4更详细的描述文字..." },
+        { "img": "images/5m.jpg", "alt": "酷站代码教程", "text": "图片5更详细的描述文字..." }
+    ],
+    "lunbo2": [1, 2],
+    "tuijian": [1, 2, 3, 4, 5],
+    "dianji": [1, 2, 3, 4, 5],
+    "zuixin": [1, 2, 3, 4, 5],
+    "tuwentuijian": [1, 2, 3, 4, 5],
+    "youqinglianjie": [
+        { "name": "百度", "url": "www.baidu.com" },
+        { "name": "google", "url": "www.google.com" }
+    ],
+    "biaoqian": ["手机配件", "手机", "苹果Apple", "SumSung", "Sony", "华维荣耀", "手机", "三星", "索尼", "华维荣耀", "三星", "索尼"],
+    "guanggao": [
+        { "name": "广告1", "img": "images/ad300x100.jpg", "descr": "描述..." },
+        { "name": "广告2", "img": "images/03.jpg", "descr": "描述..." }
+    ],
+    "footer": { "copyright": " Copyright 2018", "img": "images/ftlogo.png", "descr": "时尚设计，国内最优化的模板设计平台!" },
+    "header": {
+        "top": [
+            { "url": "/", "text": "Home" },
+            { "url": "/", "text": "官方微博" },
+            { "url": "/", "text": "关于我们" }
+        ],
+        "logo": "images/logo.png",
+        "search": { "url": "/", "img": "images/search.png" },
+        "nav": [
+            { "url": "/", "text": "网站首页" },
+            { "url": "list.html", "text": "新闻中心" },
+            { "url": "/", "text": "产品中心" },
+            { "url": "/", "text": "关于我们" },
+            { "url": "/", "text": "联系我们" }
+        ]
+    }
+}
---
+++ js/header.js
@@ -0,0 +1,35 @@
+﻿//header.js
+var cheader = config.header;
+var header = document.getElementById("header");
+var topbg = document.createElement("div"); topbg.setAttribute("class", "topbg");
+var topbgul = document.createElement("ul"); topbgul.setAttribute("class", "topnav");
+for (var i in cheader.top) {
+    var topnava = document.createElement("a"); topnava.href = cheader.top[i].url; topnava.innerText = cheader.top[i].text;
+    topbgul.appendChild(topnava);
+}
+topbg.appendChild(topbgul);
+header.appendChild(topbg);
+var topbgline = document.createElement("div"); topbgline.setAttribute("class", "topbgline");
+header.appendChild(topbgline);
+var logo = document.createElement("div"); logo.setAttribute("class", "logo");
+var logo_l = document.createElement("div"); logo_l.setAttribute("class", "logo_l f_l");
+var logo_la = document.createElement("a"); logo_la.href = "/";
+var logo_laimg = document.createElement("img"); logo_laimg.src = cheader.logo;
+logo_la.appendChild(logo_laimg);
+logo_l.appendChild(logo_la);
+logo.appendChild(logo_l);
+var logo_r = document.createElement("div"); logo_r.setAttribute("class", "logo_r f_r");
+var logo_ra = document.createElement("a"); logo_ra.href = cheader.search.url;
+var logo_raimg = document.createElement("img"); logo_raimg.src = cheader.search.img;
+logo_ra.appendChild(logo_raimg);
+logo_r.appendChild(logo_ra);
+logo.appendChild(logo_r);
+header.appendChild(logo);
+var topnav = document.createElement("nav"); topnav.setAttribute("id", "topnav");
+var topnavul = document.createElement("ul");
+for (var i in cheader.nav) {
+    var topnava = document.createElement("a"); topnava.href = cheader.nav[i].url; topnava.innerText = cheader.nav[i].text; topnava.setAttribute("target", "_blank");
+    topnavul.appendChild(topnava);
+}
+topnav.appendChild(topnavul);
+header.appendChild(topnav);
